d6fa7d8fda4d30aae5e39317dbb2f3e3b20ae741
Siva Sivapalan <ssivapaul@gmail.com>
2025-02-27 11:01:05 +1100

    Third commit

--- BuildingASpreadSheet/script.js
+++ BuildingASpreadSheet/script.js
@@ -1,4 +1,13 @@
-const range = (start, end) => Array(end - start + 1);
+const sum = (nums) => nums.reduce((acc, el) => acc + el, 0);
+
+const range = (start, end) =>
+  Array(end - start + 1)
+    .fill(start)
+    .map((element, index) => element + index);
+const charRange = (start, end) =>
+  range(start.charCodeAt(0), end.charCodeAt(0)).map((code) =>
+    String.fromCharCode(code)
+  );
 
 window.onload = () => {
   const container = document.getElementById("container");
@@ -8,4 +17,16 @@ window.onload = () => {
     label.textContent = name;
     container.appendChild(label);
   };
+  const letters = charRange("A", "J");
+  letters.forEach(createLabel);
+  range(1, 99).forEach((number) => {
+    createLabel(number);
+    letters.forEach((letter) => {
+      const input = document.createElement("input");
+      input.type = "text";
+      input.id = letter + number;
+      input.ariaLabel = letter + number;
+      container.appendChild(input);
+    });
+  });
 };
