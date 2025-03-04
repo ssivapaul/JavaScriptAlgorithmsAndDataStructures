01cf57172c873af75d2b4d74fd6eaaa2035640f2
Siva Sivapalan <ssivapaul@gmail.com>
2025-03-04 17:39:19 +1100

    Project completed

--- BuildATelephoneNumberValidator/script.js
+++ BuildATelephoneNumberValidator/script.js
@@ -1,26 +1,24 @@
 const checkBtn = document.getElementById("check-btn");
 const clearBtn = document.getElementById("clear-btn");
+const resultsDiv = document.getElementById("results-div");
+const userInput = document.getElementById("user-input");
 
 const validatePhoneNumber = (phNum) => {
-  if (!phNum) {
+  if (phNum === "") {
     alert("Please provide a phone number");
   }
+
   const phoneRegex =
     /^(?:\+?1\s?)?(?:\(\d{3}\)|\d{3})[-.\s]?\d{3}[-.\s]?\d{4}$/;
   return phoneRegex.test(phNum)
-    ? `Valid US number: <b>${phNum}</b>`
-    : `Invalid US number: <b>${phNum}</b>`;
+    ? "Valid US number: " + phNum
+    : "Invalid US number: " + phNum;
 };
 
-checkBtn.addEventListener(
-  "click",
-  () =>
-    (document.getElementById("results-div").innerHTML = validatePhoneNumber(
-      document.getElementById("user-input").value
-    ))
-);
+checkBtn.addEventListener("click", () => {
+  resultsDiv.innerHTML = validatePhoneNumber(userInput.value);
+});
 
-clearBtn.addEventListener(
-  "click",
-  () => (document.getElementById("results-div").innerHTML = "")
-);
+clearBtn.addEventListener("click", () => {
+  document.getElementById("results-div").innerHTML = "";
+});
